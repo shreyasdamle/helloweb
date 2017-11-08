cc114ea94693a74bd9e89756d3072d806204ba9d
Shreyas Damle <sdamle@mobileiron.com>
2017-11-08 15:56:20 -0800

    adding variables and tags

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # this is your new view
-    return render(request, 'index.html')
+    # defining the variable
+    number = 7478.24
+    # passing the variable to the view
+    thing = "Bitcoin"
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+    })
