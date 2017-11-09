c2db71376fcbc902dc382f0202cd9d3bd096efad
Shreyas Damle <sdamle@mobileiron.com>
2017-11-08 16:13:44 -0800

    adding dynamic data

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# import your model
+from collection.models import Cryptocurrency
+
+# set up automated slug creation
+class CryptocurrencyAdmin(admin.ModelAdmin):
+    model = Cryptocurrency
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Cryptocurrency, CryptocurrencyAdmin)
